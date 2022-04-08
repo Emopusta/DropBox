dbb0805cc684649556d5290e5be4d9c05c63afd8
Emopusta <emreduman225@hotmail.com>
2022-04-08 21:28:03 +0300

    main

--- User.py
+++ User.py
@@ -5,7 +5,7 @@ class User:
     def __init__(self,userName="",password="",oAuthKey=""):
         self.userName = userName
         self.password = password
-        self.oAuthKey = "sl.BFXkZIrqN_OGrRHq52_QGsVpxncEvypnuYy88X7TdMUAk7S_0l5rfh3xB2qjPBqJVxTquLTt2lbBHL4yK4jG62qkhlrzeTh1IvgimVqQhf2Y2F-dZpZsI4dR-wjekDgu4-GIjlrAPVi6"
+        self.oAuthKey = ""
         
 
     def GetUserName(self):

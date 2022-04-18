0de526ff8d92a1c2ba0fde9192bcd6db3e7ec21b
Emopusta <emreduman225@hotmail.com>
2022-04-19 00:22:28 +0300

    main

--- MainWindowUI.py
+++ MainWindowUI.py
@@ -32,7 +32,7 @@ class Ui_MainWindow(object):
     
     def ListItemsToListView(self):
         # we'll get these from database
-        self.user = User("Emre","asd",oAuthKey="sl.BF_2p_WIZnmdW4MIWDb6yqSDCr-f7M1B0PGIyDyzNZ1OfuP6yDWnzx1F3cVAAD7sB0WN6P642bwPfEJ47THcBBfk1MhW-axNmueWMLcAL8bRhzCP3TMQpCH5iUXm6MSxT288jw69FHuF")
+        self.user = User("Emre","asd",oAuthKey="")
         dbx = dropbox.Dropbox(self.user.oAuthKey)
         listOfNames = self.ReturnFileNames(dbx)
         self.listWidget.clear()
--- User.py
+++ User.py
@@ -5,7 +5,7 @@ class User:
     def __init__(self,userName="",password="",oAuthKey=""):
         self.userName = userName
         self.password = password
-        self.oAuthKey = "sl.BF_2p_WIZnmdW4MIWDb6yqSDCr-f7M1B0PGIyDyzNZ1OfuP6yDWnzx1F3cVAAD7sB0WN6P642bwPfEJ47THcBBfk1MhW-axNmueWMLcAL8bRhzCP3TMQpCH5iUXm6MSxT288jw69FHuF"
+        self.oAuthKey = ""
         
 
     def GetUserName(self):

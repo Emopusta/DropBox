b010e4ea3a8e9ccc964f8487d68835fb1d8443fc
Emopusta <emreduman225@hotmail.com>
2022-05-16 20:45:57 +0300

    auth code

--- User.py
+++ User.py
@@ -5,7 +5,7 @@ class User:
     def __init__(self,userName="",password="",oAuthKey=""):
         self.userName = userName
         self.password = password
-        self.oAuthKey = "sl.BHvWzbS400_s2-qitKBRsU74eW2bfnTpI0GrIZf35dHWzfcz2HArC_gFqVEYmyZif4Tpj-SOnjm0x0oMzPo2rT7jMIbWCLBBW8UDNy_RXo0fmsA8ixIXBz1OEqNnefKe1nNMDIWt3M-w"
+        self.oAuthKey = ""
         
 
     def GetUserName(self):

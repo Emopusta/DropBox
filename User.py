e1b9b2539ba3fc102cb6a6e1dcc5c37c88a42aa8
Emopusta <emreduman225@hotmail.com>
2022-05-20 16:11:49 +0300

    added report

--- User.py
+++ User.py
@@ -5,7 +5,8 @@ class User:
     def __init__(self,userName="",password="",oAuthKey=""):
         self.userName = userName
         self.password = password
-        self.oAuthKey = ""
+        self.oAuthKey = "sl.BHwZGGc8jMQcJSPVuuWMZdDDY305avXVvcr1bjSCkHK2_dwsV15tk1uTyHicBbkbdu70gsaqBeSzfio6FyA2UyA0ETLiJtjKP6WUUMWD1jy_o3KuIP2SrmutKboF6brGP5VsXeJWNT4C"
+        
         
 
     def GetUserName(self):

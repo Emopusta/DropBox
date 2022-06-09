1c8d8a4bf1951e34c5befd97ba20475a8241f1e8
Emopusta <emreduman225@hotmail.com>
2022-06-09 20:58:23 +0300

    user fixes

--- User.py
+++ User.py
@@ -3,9 +3,9 @@ import dropbox as dropbox
 
 
 class User:
-    Id = 0
-    def __init__(self,userName="",password="",oAuthKey=""):
-        
+    
+    def __init__(self,Id=None,userName="",password="",oAuthKey=""):
+        self.Id = Id 
         self.userName = userName
         self.password = password
         self.oAuthKey = "sl.BJPzQCSbwk0yZAtKfmgEDiQALUAgjfdWthIZXz7khrDe0DlruzUcQ-R2N7FiAgViJvKVzUGSsC0N8wYhPQkwY-6o8IzbcaoDZXbGMECTa9lgf07Q9hiEBQqJcJxokucug4U5xg9V3v16"
